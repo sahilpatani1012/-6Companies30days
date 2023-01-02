d756949f4706abc16c139533c2f15115264765a5
Sahil Patani <spatani9@gmail.com>
2023-01-02 15:05:19 +0530

    Number of Substrings Containing All Three Characters correction

--- SubstringsWithAll3Chars.cpp
+++ SubstringsWithAll3Chars.cpp
@@ -3,8 +3,27 @@
 #include <algorithm>
 #include <math.h>
 using namespace std;
+
+int numberOfSubstrings(string s)
+{
+    int i = 0, j = 0;
+    int res = 0;
+    int n = s.length();
+    unordered_map<char, int> m;
+    for(;j<n;j++)
+    {
+        m[s[j]]++;
+        while(m['a'] and m['b'] and m['c']){
+            m[s[i]]--;
+            i++;
+        }
+        res += i;
+    }
+    return res;
+}
+
 int main()
 {
-
+    
     return 0;
 }
